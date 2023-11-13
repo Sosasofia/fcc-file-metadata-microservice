bf8c9efb4cf260889af2cf879b807dd5b8ff30ae
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-11-13 00:13:29 -0300

    Fix solution

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
